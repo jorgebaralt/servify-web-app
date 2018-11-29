dabad3242f9bba2c3f52a5b2963ac9ee1ea5e826
Robert <rmolinamir@gmail.com>
2018-11-28 22:08:25 -0500

    Initial setup, assets folder address changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import loading from './assets/svg/loading.svg';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello world
+        <div style={{margin: '25px auto', fontSize: '100px', fontWeight: '700',textAlign: 'center'}}>Servify</div>
+        <img style={{display: 'block', margin: 'auto', width: '35vw'}} src={loading} alt='' />
       </div>
     );
   }
