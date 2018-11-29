4b5bdea7f97fd2fe206e4a061215ef4909cf4cb2
Robert <rmolinamir@gmail.com>
2018-11-29 00:11:05 -0500

    LoadingDots refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import LoadingDots from './components/LoadingDots'
+import LoadingDots from './components/LoadingDots/LoadingDots'
 
 class App extends Component {
   render() {
     return (
       <div>
         <div style={{ margin: '25px auto', fontSize: '100px', fontWeight: '700', textAlign: 'center' }}>Servify</div>
-        <LoadingDots style={{display: 'block', margin: 'auto', width: '35vw'}}/>
+        <LoadingDots width={'35vw'}/>
       </div>
     );
   }
