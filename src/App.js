61a371b71104a55d48c2cdba787fbbc8aa34865e
jorgebaralt <jorgebaraltq@gmaik.com>
2018-11-28 23:57:21 -0500

    LoadingDots component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import loading from './assets/svg/loading.svg';
+import LoadingDots from './components/LoadingDots'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div style={{margin: '25px auto', fontSize: '100px', fontWeight: '700',textAlign: 'center'}}>Servify</div>
-        <img style={{display: 'block', margin: 'auto', width: '35vw'}} src={loading} alt='' />
+        <div style={{ margin: '25px auto', fontSize: '100px', fontWeight: '700', textAlign: 'center' }}>Servify</div>
+        <LoadingDots style={{display: 'block', margin: 'auto', width: '35vw'}}/>
       </div>
     );
   }
