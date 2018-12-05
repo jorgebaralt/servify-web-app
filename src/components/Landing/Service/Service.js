2764f7df4deee3a43b42bf0aeff960436cb821a9
Robert Molina <rmolinamir@gmail.com>
2018-12-04 22:11:26 -0500

    Carousel this.setSlidesToShow and this.responsiveSlider

--- src/components/Landing/Service/Service.js
+++ src/components/Landing/Service/Service.js
@@ -4,8 +4,9 @@ import classes from './Service.module.css';
 import StarsRating from '../../../components/UI/StarsRating/StarsRating';
 
 class Service extends Component  {
-
     render () {
+        // Total rating amount, defaults to 5
+        const totalStarsRatingAmount = this.props.totalAmount ? this.props.totalAmount : 5;
         return (
             <div className={classes.Service}>
                 <a draggable="false" href='/' className={classes.Wrapper} target="_blank">
@@ -20,18 +21,28 @@ class Service extends Component  {
                 <a draggable="false" href='/' className={classes.Details} target="_blank">
                     <div>
                         <div className={classes.Header}>
-                            <span style={{color: 'rgb(131, 104, 69)'}}>{this.props.header}</span>
+                            <span style={{color: '#1fa6c8'}}>{this.props.header}</span>
                         </div>
                     </div>
+                    {/* Title */}
                     <div className={classes.Title}>{this.props.title}</div>
+                    {/* Price */}
                     <div className={classes.Price}>{this.props.price}</div>
-                    {/* Star Rating */}
+                    {/* Rating */}
                     <div>
+                        {/* Average Rating */}
                         <span role="img">
-                            <span className={classes.RatingsAvg}>{this.props.RatingsAvg}</span>
-                            <StarsRating rating={.7} />
+                            {/* Rating Average times the total amount of stars for easier interpretation */}
+                            <span className={classes.RatingsAvg}>{(this.props.ratingAvg*totalStarsRatingAmount).toFixed(2)}</span>
+                            <StarsRating rating={this.props.ratingAvg} amount={totalStarsRatingAmount}/>
                         </span>
-                        <span className={classes.RatingsAmount}><span className={classes.Amount}>{this.props.ratingsAmount}</span></span>
+                        {/* Total amount of Ratings */}
+                        <span className={classes.RatingsAmount}>
+                            <span className={classes.Amount}>
+                                {this.props.ratingAmount}
+                                {/* Dynamic string depending if 1 rating or more */}
+                                &nbsp;total rating{this.props.ratingAmount > 0 ? 's' : null}</span> 
+                        </span>
                     </div>
                 </a>
             </div>
--- src/components/UI/Carousel/Carousel.js
+++ src/components/UI/Carousel/Carousel.js
@@ -13,65 +13,120 @@ class Carousel extends Component {
         this.previous = this.previous.bind(this);
     }
         next() {
-        this.slider.slickNext();
+        this.mySlider.slickNext();
     }
         previous() {
-        this.slider.slickPrev();
+        this.mySlider.slickPrev();
+    }
+
+    /**
+     * Dynamic responsive slider, depending on the amount of children,
+     */
+    responsiveSlider = () => {
+        let responsive = [];
+        if (this.props.children.length >= 3) {
+            responsive.push(
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                }
+            );
+        };
+        if (this.props.children.length >= 2) {
+            responsive.push(
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+            );
+        };
+        responsive.push(
+            {
+                breakpoint: 350,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        );
+        return responsive;
+    };
+
+    /**
+     * Setter function that determines which breakpoint the slider is set at, to set the slidesToShow
+     * property in the state, so that we can manage when to render the Next and Previous slide buttons.
+     */
+    setSlidesToShow = () => {
+        // Pointer protection
+        // if (!this.mySlider) { return; }
+        // For every object containing the breakpoints in the responsive array set and sent to the slider
+        for (let breakpoint in this.mySlider.props.responsive) {
+            /**
+             * We're looping to find the right breakpoint and extract the slidesToShow property to set
+             * the state. If the loop breakpoint equals the current slider's state breakpoint then
+             * it will set the state's slidesToShow equal to the current slider's slidesToShow, then
+             * break the loop.
+             */
+            if (this.mySlider.props.responsive[breakpoint].breakpoint === this.mySlider.state.breakpoint) {
+                const slidesToShow = this.mySlider.props.responsive[breakpoint].settings.slidesToShow;
+                this.setState( () => {
+                    return {slidesToShow}
+                });
+                break;
+            }
+        };
     }
 
     state = {
         activeSlide: 0,
-        activeSlide2: 0
+        activeSlide2: 0,
+        slidesToShow: 4, // Default, if on mobile or smaller screens it will be reapplied
+        responsive: this.responsiveSlider(),
     };
 
+    componentDidMount () {
+        window.onresize = () => {
+            this.setSlidesToShow();
+        };
+    }
+
+    // Update only at the end or beginning of the carousel
+    shouldComponentUpdate () {
+        if (this.state.activeSlide === 0 || this.state.activeSlide + this.state.slidesToShow >= this.props.children.length) {
+            return true;
+        }
+        return false;
+    }
+
     render () {
-        // let reactSwipeEl;
+        // Carousel default settings buildup.
         let settings = {
             dots: false,
             infinite: false,
-            // TODO connect to global reducer to set true if on mobile
-            swipeToSlide: false,
-            speed: 500,
+            arrows: false,
+            swipeToSlide: true,
+            draggable: false,
+            speed: 250,
             slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToScroll: 1,
             initialSlide: 0,
             beforeChange: (current, next) => this.setState({ activeSlide: next }),
             afterChange: current => this.setState({ activeSlide2: current }),
-            responsive: [
-                {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-                },
-                {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-                },
-                {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-                }
-            ]
+            responsive: this.state.responsive
         };
+        // If there are settings passed by a prop use those.
         if (this.props.settings) {
             settings = this.props.settings;
         }
         return (
             <div className={classes.Carousel}>
-                <Slider  ref={ref => (this.slider = ref)} {...settings}>
+                <Slider  ref={ref => (this.mySlider = ref)} {...settings}>
                     {this.props.children} 
                 </Slider>
+                {/* PrevButton */}
                 {this.state.activeSlide === 0 ? 
                     null : 
                     <div className={classes.PrevButton}>
@@ -79,7 +134,8 @@ class Carousel extends Component {
                         </button>
                     </div>
                 }
-                {this.state.activeSlide === this.props.children.length - 1 ? 
+                {/* NextButton */}
+                {this.state.activeSlide + this.state.slidesToShow >= this.props.children.length  || this.state.slidesToShow >= this.props.children.length  ? 
                     null : 
                     <div className={classes.NextButton}>
                         <button className="button" onClick={this.next}>
