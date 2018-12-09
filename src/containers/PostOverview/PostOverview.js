5eb6dc6d9529fcb74f4d8e778e8ab67d191a1daf
jorgebaralt <jorgebaraltq@gmaik.com>
2018-12-09 18:01:12 -0500

    What users see, responsive added.

--- src/containers/PostOverview/PostOverview.js
+++ src/containers/PostOverview/PostOverview.js
@@ -3,6 +3,7 @@ import classes from './PostOverview.module.css';
 import HeaderImage from '../../assets/images/overview.jpeg';
 import Button from '../../components/UI/Button/Button';
 import { One, Two, Three, Four } from '../../assets/svg/Numbers';
+import {ListStar} from '../../assets/svg'
 
 class PostOverview extends Component {
 	render() {
@@ -26,7 +27,7 @@ class PostOverview extends Component {
 				{/* - why post a service  - why we are the best option*/}
 				<div className={classes.WhyContainer}>
 					<div className={classes.WhyDiv}>
-						<h2 className={classes.WhyTitle}>Why pick Servify?</h2>
+						<h2 className={classes.SecondTitle}>Why pick Servify?</h2>
 						<p className={classes.WhyText}>
 							Servify allow users to post any type of service free
 							of price in a simple and easy way. You have full
@@ -34,7 +35,7 @@ class PostOverview extends Component {
 						</p>
 					</div>
 					<div className={classes.WhyDiv}>
-						<h2 className={classes.WhyTitle}>Why we do this?</h2>
+						<h2 className={classes.SecondTitle}>Why we do this?</h2>
 						<p className={classes.WhyText}>
 							We want to privde a platform where users can come
 							and find services and an easy and friendly way. Let
@@ -44,34 +45,53 @@ class PostOverview extends Component {
 				</div>
 				{/* Steps to create a service */}
 
-				<h1 className={classes.HowToPostTitle}>
+				<h1 className={classes.Title}>
 					How to post a service
 				</h1>
 				<div className={classes.HowToPostContainer}>
 					<div className={classes.HowToPostStep}>
 						<One style={{ width: 50, height: 50 }} />
-						<h2 className={classes.HowToPostStepTitle}>Create an account</h2>
+						<h2 className={classes.SecondTitle}>Create an account</h2>
 						<p className={classes.HowToPostText}>Before posting a service, make sure yo create an account</p>
 					</div>
 					<div className={classes.HowToPostStep}>
 						<Two style={{ width: 50, height: 50 }} />
-						<h2 className={classes.HowToPostStepTitle}>Fill your information</h2>
+						<h2 className={classes.SecondTitle}>Fill your information</h2>
 						<p className={classes.HowToPostText}>Once you have an account, you will be able to post as many services as you want</p>
 					</div>
 					<div className={classes.HowToPostStep}>
 						<Three style={{ width: 50, height: 50 }} />
-						<h2 className={classes.HowToPostStepTitle}>Post your service</h2>
+						<h2 className={classes.SecondTitle}>Post your service</h2>
 						<p className={classes.HowToPostText}>After filling all your information, post your service so everyone is able to look at it.</p>
 					</div>
 					<div className={classes.HowToPostStep}>
 						<Four style={{ width: 50, height: 50 }} />
-						<h2 className={classes.HowToPostStepTitle}>Get contacted for jobs</h2>
+						<h2 className={classes.SecondTitle}>Get contacted for jobs</h2>
 						<p className={classes.HowToPostText}>Once your service is posted, users will be able to contact you to schedule a job.</p>
 					</div>
 				</div>
+
 				{/* How we display info (what clients see) */}
+				<h1 className={classes.Title}>
+					What users see
+				</h1>
+				<div className={classes.WhatUsersSeeContainer}>
+					<div className={classes.WhatUsersSeeImgContainer}>
+						<img className={classes.WhatUsersSeeImg} src={require('../../assets/images/happy.jpg')} alt="" />
+					</div>
+					<div className={classes.WhatUsersSeeDescriptionContainer}>
+						<ul className={classes.ListContainer}>
+							<li className={classes.ListItem}> <ListStar style={{ width: 24, height: 24}}/> Reviews and rating of your service</li>
+							<li className={classes.ListItem}> <ListStar style={{ width: 24, height: 24}}/> Service category and information</li>
+							<li className={classes.ListItem}> <ListStar style={{ width: 24, height: 24}}/> Your contact information</li>
+							<li className={classes.ListItem}> <ListStar style={{ width: 24, height: 24}}/> Location of your service (Map)</li>
+							<li className={classes.ListItem}> <ListStar style={{ width: 24, height: 24 }} /> Easy way to contact you</li>
+							<li className={classes.ListItem}> <ListStar style={{ width: 24, height: 24 }} /> Option to add to favorites</li>
+						</ul>
+					</div>
+				</div>
 				{/* Client payment options */}
-				{/* Our price -> Free for now */}
+
 				{/* Company growth */}
 			</>
 		);
