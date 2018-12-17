c3397ac6c963e2dfdcdec926b2192e4c39eee9f0
Robert <rmolinamir@gmail.com>
2018-12-17 15:21:13 -0500

    Compose enhancers fix for mobile and other browsers

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import * as sagas from './store/sagas/';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : null ;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
