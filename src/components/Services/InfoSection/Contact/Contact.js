2c3facae35af30ee41941e0e26ddde3b2922bb7a
Robert <rmolinamir@gmail.com>
2019-02-10 18:55:27 -0500

    Services/Contact.js made a functional hooked component

--- src/components/Services/InfoSection/Contact/Contact.js
+++ src/components/Services/InfoSection/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 // CSS
 import classes from './Contact.module.css';
 // JSX
@@ -7,92 +7,77 @@ import SVG from '../../../SVG/SVG';
 import Separator from '../../../UI/Separator/Separator';
 import CopyText from '../../../UI/CopyText/CopyText';
 
-class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.myEmail = React.createRef();
-        this.myPhone = React.createRef();
-        this.state = {
-            bIsHidden: true,
-            phone: props.phone,
-            email: props.email
-        }
+const contact = (props) => {
+    const [bIsHidden, setIsHidden] = useState(true);
+    const myPhone = useRef(null);
+    const myEmail = useRef(null);
+
+    const toggleModal = () => {
+        setIsHidden(!bIsHidden);
     }
 
-    closeModal = () => {
-        this.setState({
-            bIsHidden: true
-        });
+    const closeModal = () => {
+        setIsHidden(true);
     }
 
-    toggleModal = () => {
-        this.setState(prevState => {
-            return {
-                bIsHidden: !prevState.bIsHidden
-            }
-        })
-    }
-
-    render () {
-        if (!this.props.contact) { return null; } // Protection
-        if (!this.props.contact.phone && !this.props.contact.email) { return null; } // No need to render anything if no email and phone.
-        return (
-            <>
-                <Modal 
-                    maxWidth={500}
-                    toggleModal={this.toggleModal}
-                    closeModal={this.closeModal}
-                    show={!this.state.bIsHidden}>
-                    <h1 className={classes.Title}>Contact Information</h1>
-                    <Separator />
-                    {/* Phone */}
-                    {this.props.contact.phone ? 
+    if (!props.contact) { return null; } // Protection
+    if (!props.contact.phone && !props.contact.email) { return null; } // No need to render anything if no email and phone.
+    return (
+        <>
+            <Modal 
+                maxWidth={500}
+                toggleModal={toggleModal}
+                closeModal={closeModal}
+                show={!bIsHidden}>
+                <h1 className={classes.Title}>Contact Information</h1>
+                <Separator />
+                {/* Phone */}
+                {props.contact.phone ? 
+                    <div className={classes.Container}>
+                        <div className={classes.Content}>
+                            <div className={classes.Contact}>
+                                <SVG svg='phone' />
+                                <input 
+                                    readOnly
+                                    value={props.contact.phone} 
+                                    ref={myPhone} 
+                                    className={classes.Input} />
+                            </div>
+                        </div>
+                        <CopyText text={'Copy phone'} 
+                            copyReference={myPhone} />
+                    </div>
+                    : null}
+                {/* Email */}
+                {props.contact.email ? 
+                    <>
                         <div className={classes.Container}>
                             <div className={classes.Content}>
                                 <div className={classes.Contact}>
-                                    <SVG svg='phone' />
+                                    <SVG svg='envelope' />
                                     <input 
                                         readOnly
-                                        value={this.props.contact.phone} 
-                                        ref={this.myPhone} 
+                                        value={props.contact.email} 
+                                        ref={myEmail}
                                         className={classes.Input} />
                                 </div>
                             </div>
-                            <CopyText text={'Copy phone'} 
-                                copyReference={this.myPhone} />
+                            <CopyText text={'Copy email'} 
+                                copyReference={myEmail} />
                         </div>
-                        : null}
-                    {/* Email */}
-                    {this.props.contact.email ? 
-                        <>
-                            <div className={classes.Container}>
-                                <div className={classes.Content}>
-                                    <div className={classes.Contact}>
-                                        <SVG svg='envelope' />
-                                        <input 
-                                            readOnly
-                                            value={this.props.contact.email} 
-                                            ref={this.myEmail}
-                                            className={classes.Input} />
-                                    </div>
-                                </div>
-                                <CopyText text={'Copy email'} 
-                                    copyReference={this.myEmail} />
-                            </div>
-                            <Separator />
-                            <div className={classes.Container}>
-                                <a className={classes.Email} href={['mailto:', this.props.contact.email].join('')}>
-                                    <SVG svg='sent-envelope' />
-                                    <span>Send an email</span>
-                                </a>
-                            </div>
-                        </>
-                        : null}
-                </Modal>
-                <button type="button" onClick={this.toggleModal} className={classes.Button} aria-busy="false">Contact provider</button> 
-            </>
-        )
-    }
+                        <Separator />
+                        <div className={classes.Container}>
+                            <a className={classes.Email} href={['mailto:', props.contact.email].join('')}>
+                                <SVG svg='sent-envelope' />
+                                <span>Send an email</span>
+                            </a>
+                        </div>
+                    </>
+                    : null}
+            </Modal>
+            <button type="button" onClick={toggleModal} className={classes.Button} aria-busy="false">Contact provider</button> 
+        </>
+    );
 }
 
-export default Contact;
+export default contact;
