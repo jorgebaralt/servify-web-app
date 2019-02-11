7885b885bd7f65a61db18475c8f4d58f8e4808d5
Robert <rmolinamir@gmail.com>
2019-02-10 20:10:39 -0500

    CopyText.js turned into a functional 'hooked' component, CSS fix for Mac OS

--- src/components/UI/CopyText/CopyText.js
+++ src/components/UI/CopyText/CopyText.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React, { useRef, useState } from 'react';
 // CSS
 import classes from './CopyText.module.css';
 
-class CopyText extends Component {
-    constructor(props) {
-        super(props);
-        this.myTooltip = React.createRef();
-    }
+const copyText = (props) => {
+    const myTooltip = useRef(null);
+    const [text, setText] = useState('Copy to clipboard');
+    const [buttonText] = useState(props.text ? props.text : 'Copy text');
 
-    state = {
-        text: 'Copy to clipboard',
-        buttonText: this.props.text ? this.props.text : 'Copy text'
-    }
-
-    copyText = (event) => {
-        if (!this.props.copyReference) { return; }
-        const copyText = this.props.copyReference.current;
+    const copyText = () => {
+        if (!props.copyReference) { return; }
+        const copyText = props.copyReference.current;
         if (!copyText.select) { return; }
         copyText.select();
         // Copying selected text
@@ -25,44 +19,31 @@ class CopyText extends Component {
         } else {
             document.selection.empty();
         }
-        // event.target.focus();
-        this.setState({
-            text: ['Copied:',copyText.value].join(' ')
-        })
+        setText(['Copied:',copyText.value].join(' '));
     }
 
-    onMouseLeave = () => {
-        this.myTooltip.current.innerHTML = "Copy to clipboard";
-        this.setState({
-            text: 'Copy to clipboard'
-        })
+    const onMouseLeaveHandler = () => {
+        myTooltip.current.innerHTML = "Copy to clipboard";
+        setText('Copy to clipboard');
     }
 
-    onCopyHandler = (event) => {
-        console.log('ping')
-        this.copyText(event)
-    }
-
-    render() {
-        // Logical shortcut for only displaying the 
-        // button if the copy command exists.
-        if (!document.queryCommandSupported('copy')) { return null; }
-        return (
-            <div className={classes.Container}>
-                <button tabIndex="-1" 
-                    type='button'
-                    className={classes.Button}
-                    onClick={this.onCopyHandler} 
-                    onMouseLeave={this.onMouseLeave}>
-                    {/* Tooltip, appears on hover. */}
-                    <span ref={this.myTooltip} className={classes.Tooltip}>{this.state.text}</span>
-                    {/* Button content */}
-                    <span className={classes.Text}>{this.state.buttonText}</span>
-                </button>
-            </div>
-        );
-    }
+    // Logical shortcut for only displaying the 
+    // button if the copy command exists.
+    if (!document.queryCommandSupported('copy')) { return null; }
+    return (
+        <div className={classes.Container}>
+            <button tabIndex="-1" 
+                type='button'
+                className={classes.Button}
+                onClick={copyText} 
+                onMouseLeave={onMouseLeaveHandler}>
+                {/* Tooltip, appears on hover. */}
+                <span ref={myTooltip} className={classes.Tooltip}>{text}</span>
+                {/* Button content */}
+                <span className={classes.Text}>{buttonText}</span>
+            </button>
+        </div>
+    );
 }
 
-
-export default CopyText;
+export default copyText;
